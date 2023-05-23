56f305509cd7abfb656630c964f7d2e86058cf9c
DataVerpalenScorpian <99246618+DataVerpalenScorpian@users.noreply.github.com>
2023-05-23 12:51:28 +0200

    Name's aangepast

---
+++ verkordform.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<h1>Klant</h1>
+	<h2>Toevoegen</h2>
+	<form method="post" action="">
+        <br>   
+    <label for="an">Verkorddatum:</label>
+    <input type="text" id="" name="verkorddatum" placeholder="verkorddatum" required/>
+        <br>
+    <label for="an">Verkordbestaantal:</label>
+    <input type="text" id="" name="verkordbestaantal" placeholder="verkordbestaantal" required/>
+        <br>
+    <label for="an">Verkordstatus:</label>
+    <input type="text" id="" name="verkordstatus" placeholder="verkordstatus" required/>
+        <br>
+    </form></br>
+
+	<a href='index.php'>Terug</a>
+
+</body>
+</html>
+
