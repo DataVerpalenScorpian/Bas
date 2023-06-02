51dfa4aae88841fd053ded84d698868d7c0116f0
DataVerpalenScorpian <99246618+DataVerpalenScorpian@users.noreply.github.com>
2023-06-02 23:57:51 +0200

    Insert toegevoegd in verkooporder

--- insert_verkooporders.php
+++ insert_verkooporders.php
@@ -1,40 +1,23 @@
 <?php
-include 'conn.php';
 include 'verkooporder.php';
 
-// ... rest van de code ...
-
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Verkooporder toevoegen</title>
+    <title>Verkooporder</title>
 </head>
 <body>
-    <h1>Verkooporder toevoegen</h1>
     <form method="POST" action="">
-        <label>Klant ID:</label>
-        <?php
-        $verkooporder = new Verkooporder();
-        echo $verkooporder->getKlantDropdown();
-        ?>
-        <br>
-        <label>Artikel ID:</label>
-        <?php
-        echo $verkooporder->getArtikelDropdown();
-        ?>
-        <br>
-        <label>Verkooporderdatum:</label>
-        <input type="text" name="verkorddatum" required>
-        <br>
-        <label>Aantal:</label>
-        <input type="text" name="verkordbestaantal" required>
-        <br>
-        <label>Status:</label>
-        <input type="text" name="verkordstatus" required>
-        <br>
-        <input type="submit" name="insert" value="Verkooporder toevoegen">
+        Verkooporder ID: <input type="text" name="verkordid"><br>
+        Klant: <?php echo $verkooporder->getKlantDropdown(); ?><br>
+        Artikel: <?php echo $verkooporder->getArtikelDropdown(); ?><br>
+        Datum: <input type="text" name="verkorddatum"><br>
+        Bestelhoeveelheid: <input type="text" name="verkordbestaantal"><br>
+        Status: <input type="text" name="verkordstatus"><br>
+        <input type="submit" name="insert" value="Insert">
     </form>
 </body>
 </html>
--- verkooporder.php
+++ verkooporder.php
@@ -1,60 +1,95 @@
 <?php
+include 'conn.php';
 include 'Config.php';
 
 class Verkooporder extends Config {
 
-    public function getKlantDropdown() {
-        try {
-            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $sql = "SELECT klantid, klantnaam FROM klanten";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $klanten = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            $dropdown = '<select name="klantid">';
-            foreach ($klanten as $klant) {
-                $klantid = $klant['klantid'];
-                $klantnaam = $klant['klantnaam'];
-                $dropdown .= "<option value=\"$klantid\">$klantnaam</option>";
-            }
-            $dropdown .= '</select>';
-
-            return $dropdown;
-        } catch (PDOException $e) {
-            echo "Fout bij het ophalen van klanten: " . $e->getMessage();
-            return '';
+    public function insert($verkordid, $artid, $klantid, $verkorddatum, $verkordbestaantal, $verkordstatus) {
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        if ($conn->connect_error) {
+            die("Kan geen verbinding maken met de database: " . $conn->connect_error);
         }
 
-        $conn = null;
+        // Controleer op lege velden
+        if (empty($verkordid) || empty($artid) || empty($klantid) || empty($verkorddatum) || empty($verkordbestaantal) || empty($verkordstatus)) {
+            echo "Fout: Alle velden moeten worden ingevuld.";
+            return;
+        }
+
+        // Controleer of numerieke waarden correct zijn
+        if (!is_numeric($verkordid) || !is_numeric($artid) || !is_numeric($klantid) || !is_numeric($verkordbestaantal)) {
+            echo "Fout: Ongeldige numerieke waarden voor velden.";
+            return;
+        }
+
+        $sql = "INSERT INTO verkooporders (verkordid, artid, klantid, verkorddatum, verkordbestaantal, verkordstatus) VALUES ('$verkordid', '$artid', '$klantid', '$verkorddatum', '$verkordbestaantal', '$verkordstatus')";
+
+        if ($conn->query($sql) === TRUE) {
+            $verkordid = $conn->insert_id; // Haal het ingevoegde verkordid op
+            echo "Verkooporder succesvol toegevoegd. Verkooporder ID: " . $verkordid;
+        } else {
+            echo "Fout bij het toevoegen van de verkooporder: " . $conn->error;
+        }
+
+        $conn->close();
+    }
+
+    public function getKlantDropdown() {
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        if ($conn->connect_error) {
+            die("Kan geen verbinding maken met de database: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT klantid, klantnaam FROM klanten";
+        $result = $conn->query($sql);
+
+        $dropdown = '<select name="klantid">';
+        while ($row = $result->fetch_assoc()) {
+            $klantid = $row['klantid'];
+            $klantnaam = $row['klantnaam'];
+            $dropdown .= "<option value=\"$klantid\">$klantnaam</option>";
+        }
+        $dropdown .= '</select>';
+
+        $conn->close();
+
+        return $dropdown;
     }
 
     public function getArtikelDropdown() {
-        try {
-            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $sql = "SELECT artid, artikelenomschrijving FROM artikelen";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $artikelen = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            $dropdown = '<select name="artid">';
-            foreach ($artikelen as $artikel) {
-                $artid = $artikel['artid'];
-                $artikelnaam = $artikel['artikelenomschrijving'];
-                $dropdown .= "<option value=\"$artid\">$artikelnaam</option>";
-            }
-            $dropdown .= '</select>';
-
-            return $dropdown;
-        } catch (PDOException $e) {
-            echo "Fout bij het ophalen van artikelen: " . $e->getMessage();
-            return '';
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        if ($conn->connect_error) {
+            die("Kan geen verbinding maken met de database: " . $conn->connect_error);
         }
 
-        $conn = null;
+        $sql = "SELECT artid, artikelenomschrijving FROM artikelen";
+        $result = $conn->query($sql);
+
+        $dropdown = '<select name="artid">';
+        while ($row = $result->fetch_assoc()) {
+            $artid = $row['artid'];
+            $artikelnaam = $row['artikelenomschrijving'];
+            $dropdown .= "<option value=\"$artid\">$artikelnaam</option>";
+        }
+        $dropdown .= '</select>';
+
+        $conn->close();
+
+        return $dropdown;
     }
 }
+
+$verkooporder = new Verkooporder();
+
+if (isset($_POST['insert'])) {
+    $verkordid = $_POST['verkordid'] ?? '';
+    $artid = $_POST['artid'] ?? '';
+    $klantid = $_POST['klantid'] ?? '';
+    $verkorddatum = $_POST['verkorddatum'] ?? '';
+    $verkordbestaantal = $_POST['verkordbestaantal'] ?? '';
+    $verkordstatus = $_POST['verkordstatus'] ?? '';
+
+    $verkooporder->insert($verkordid, $artid, $klantid, $verkorddatum, $verkordbestaantal, $verkordstatus);
+}
+
 ?>
